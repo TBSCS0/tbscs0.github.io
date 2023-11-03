ac6ff2f85add95aeec18b35b2d21739fcd8652bc
oscarneiva <oscarneivaeneto@gmail.com>
2023-11-03 17:14:53 -0300

    duck is moving properly

--- moveDuck.js
+++ moveDuck.js
@@ -1,8 +1,8 @@
 const duckWalk = document.getElementById('duckWalk');
 
-const speed = 0.5; // Pixels to move in each step
-const screenWidth = window.innerWidth - duckWalk.clientWidth; // Width of the screen minus duck's width
-const screenHeight = window.innerHeight - duckWalk.clientHeight; // Height of the screen minus duck's height
+const speed = 0.5;
+const screenWidth = window.innerWidth - duckWalk.clientWidth;
+const screenHeight = window.innerHeight - duckWalk.clientHeight;
 
 let x = getRandomInt(0, screenWidth);
 let y = getRandomInt(0, screenHeight);
@@ -16,11 +16,9 @@ function getRandomInt(min, max) {
 }
 
 function moveDuck() {
-    // Update the duck's position
     x += speed * xDirection;
     y += speed * yDirection;
 
-    // Check if the duck has reached the screen boundaries
     if (x + duckWalk.clientWidth > screenWidth || x < 0) {
         xDirection *= -1;
     }
@@ -28,16 +26,15 @@ function moveDuck() {
         yDirection *= -1;
     }
 
-    let rand = getRandomInt(1,100);
-    if(rand % 97 === 0 && rand % 2 === 0){
+    let randOne = getRandomInt(1,100);
+    let randTwo = getRandomInt(1,2);
+    if(randOne % 97 === 0 && randTwo % 2 === 0){
         xDirection *= -1;
     }
-    if(rand % 97 === 0 && rand % 2 !== 0){
+    if(randOne % 97 === 0 && randTwo % 2 !== 0){
         yDirection *= -1;
     }
 
-
-    // Apply the new position to the duck
     duckWalk.style.left = x + 'px';
     duckWalk.style.top = y + 'px';
 
